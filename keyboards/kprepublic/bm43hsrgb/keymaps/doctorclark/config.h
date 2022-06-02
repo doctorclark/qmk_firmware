220820309ad6c6ab77de1c49be43894496192c58
doctorclark <106190893+doctorclark@users.noreply.github.com>
2022-06-02 10:10:07 -0700

    Update config.h

--- keyboards/kprepublic/bm43hsrgb/keymaps/doctorclark/config.h
+++ keyboards/kprepublic/bm43hsrgb/keymaps/doctorclark/config.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2021 bitstarr
+Edited from previous version which was Copyright 2021 bitstarr
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
